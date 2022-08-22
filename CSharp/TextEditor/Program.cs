eac6133ec417574ee12388e2f4f2b25348baacb6
Viola <violaontheroad@gmail.com>
2022-08-22 12:28:18 -0300

    construção do Menu

---
+++ CSharp/TextEditor/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("What do you want to do?");
+        Console.WriteLine("1 - Open a file");
+        Console.WriteLine("2 - Create a new file");       
+        Console.WriteLine("0 - Exit");
+        short option = short.Parse(Console.ReadLine());
+
+        switch(option)
+        {   
+            case 0: System.Environment.Exit(0); break;
+            case 1: Open(); break;
+            case 2: Edit(); break;
+
+        }
+    }
+
+    static void Open(){}
+
+    static void Edit(){}
+
+}

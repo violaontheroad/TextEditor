18145e0acce1991d57bd32402eb422370f3c9d0e
Viola <violaontheroad@gmail.com>
2022-08-23 10:44:55 -0300

    finalização função Edit

--- CSharp/TextEditor/Program.cs
+++ CSharp/TextEditor/Program.cs
@@ -1,40 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-Menu();
-
- static void Menu()
+﻿internal class Program
+{
+    private static void Main(string[] args)
     {
-        Console.Clear();
-        Console.WriteLine("What do you want to do?");
-        Console.WriteLine("1 - Open a file");
-        Console.WriteLine("2 - Create a new file");       
-        Console.WriteLine("0 - Exit");
-        short option = short.Parse(Console.ReadLine());
+        Console.WriteLine("Hello, World!");
 
-        switch(option)
-        {   
-            case 0: System.Environment.Exit(0); break;
-            case 1: Open(); break;
-            case 2: Edit(); break;
-        }
-    }
+        Menu();
 
-    static void Open(){}
-
-    static void Edit()
-    {
-        Console.Clear();
-        Console.WriteLine("Digite seu texto abaixo (or ESC to exit)");
-        Console.WriteLine("----------------------------------------");
-        string text = "";
-
-        do 
+        static void Menu()
         {
-            text += Console.ReadLine();
-            text += Environment.NewLine; // quebrar a linha no fim de cada leitura
-        }
-        while(Console.ReadKey().Key != ConsoleKey.Escape); // ConsoleKey.Escape = tecla ESC
+            Console.Clear();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Open a file");
+            Console.WriteLine("2 - Create a new file");
+            Console.WriteLine("0 - Exit");
+            short option = short.Parse(Console.ReadLine());
 
-        Console.Write(text);
+            switch (option)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+            }
+        }
+
+        static void Open() { }
+
+        static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (or ESC to exit)");
+            Console.WriteLine("----------------------------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine; // quebrar a linha no fim de cada leitura
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape); // ConsoleKey.Escape = tecla ESC
+
+            Console.Write(text);
+        }
     }
+}
